cd75c1a397686565aa1a9729f0bdee84f4e2d39d
walkingsage <walkingsage@mail.ru>
2020-08-20 02:38:39 +0300

    eeeeeeeeeeee adaptive slider

--- script/script.js
+++ script/script.js
@@ -4,8 +4,9 @@ fitnesChoise = document.getElementById('fitness__choise'),
 foodChoise = document.getElementById('food__choise'),
 toFitness = document.querySelector('.to__fitnes'),
 toFood = document.querySelector('.to__food'),
-slider = document.querySelectorAll('.zoom');
-
+slider = document.querySelectorAll('.zoom'),
+galleryFood = document.querySelectorAll('.gallery__food > img'),
+gallerFitnes = document.querySelectorAll('gallery__fitnes > img');
 
 const hide = function(elem){
     elem.style.display = 'none';
@@ -14,12 +15,35 @@ const hide = function(elem){
 const show = function(elem){
     elem.style.display = 'grid';
 };
+let src, index;
+
+
+galleryFood.forEach((element,i) => {
+    element.addEventListener('click',() => {
+        console.log(element.getAttribute('src'));
+        src = element.getAttribute('src');
+        for (let i = 0; i<galleryFood.length;i++){
+            if (galleryFood[i].getAttribute('src') === src)
+            {
+                index = i;
+            }
+            else{
+                continue;
+            }
+        };
+    });
+});
+
+
+
+
 
 toFitness.addEventListener('click', () => {
     hide(Food);
     show(Fitnes);
     toFitness.classList.add('active');
     toFood.classList.remove('active');
+    console.log(src,index);
 });
 
 fitnesChoise.addEventListener('click', () => {
