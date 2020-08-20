d06f4252a7e2a93e4d6ec7571c2dd1f85a2e6693
walkingsage <walkingsage@mail.ru>
2020-08-20 03:20:39 +0300

    adaptive slider complete

--- script/script.js
+++ script/script.js
@@ -4,9 +4,14 @@ fitnesChoise = document.getElementById('fitness__choise'),
 foodChoise = document.getElementById('food__choise'),
 toFitness = document.querySelector('.to__fitnes'),
 toFood = document.querySelector('.to__food'),
-slider = document.querySelectorAll('.zoom'),
+slider = document.querySelector('.zoom'),
+sliderImg = document.querySelector('.zoom > img'),
+prev = document.querySelector('.prev'),
+next = document.querySelector('.next'),
+close = document.querySelector('.close'),
+header = document.querySelector('.header'),
 galleryFood = document.querySelectorAll('.gallery__food > img'),
-gallerFitnes = document.querySelectorAll('gallery__fitnes > img');
+galleryFitnes = document.querySelectorAll('.gallery__fitnes > img');
 
 const hide = function(elem){
     elem.style.display = 'none';
@@ -17,10 +22,25 @@ const show = function(elem){
 };
 let src, index;
 
+const zoom = elem => {
+    hide(header);
+    sliderImg.src = src;
+    show(slider);
+};
 
-galleryFood.forEach((element,i) => {
+const zoomFitness = () => {
+    src = galleryFitnes[index].getAttribute('src');
+    sliderImg.src = src;
+};
+
+const zoomFood = () => {
+    src = galleryFood[index].getAttribute('src');
+    sliderImg.src = src;
+};
+
+
+galleryFood.forEach((element) => {
     element.addEventListener('click',() => {
-        console.log(element.getAttribute('src'));
         src = element.getAttribute('src');
         for (let i = 0; i<galleryFood.length;i++){
             if (galleryFood[i].getAttribute('src') === src)
@@ -31,12 +51,76 @@ galleryFood.forEach((element,i) => {
                 continue;
             }
         };
+        zoom(slider);
     });
 });
 
+galleryFitnes.forEach((element) => {
+    element.addEventListener('click',() => {
+        src = element.getAttribute('src');
+        for (let i = 0; i<galleryFitnes.length;i++){
+            if (galleryFitnes[i].getAttribute('src') === src)
+            {
+                index = i;
+            }
+            else{
+                continue;
+            }
+        };
+        zoom(slider);
+    });
+});
 
+prev.addEventListener('click',() => {
+    if(toFitness.classList.contains('active')){
+        if (index == 0){
+            index = galleryFitnes.length-1;
+            zoomFitness();
+        }
+        else{
+            index --;
+            zoomFitness();
+        }
+    }
+    else{
+        if (index == 0){
+            index = galleryFood.length-1;
+            zoomFood();
+        }
+        else{
+            index --;
+            zoomFood();
+        }
+    }
+});
 
+next.addEventListener('click',() => {
+    if(toFitness.classList.contains('active')){
+        if (index == galleryFitnes.length-1){
+            index = 0;
+            zoomFitness();
+        }
+        else{
+            index ++;
+            zoomFitness();
+        }
+    }
+    else{
+        if (index == galleryFood.length-1){
+            index = 0;
+            zoomFood();
+        }
+        else{
+            index ++;
+            zoomFood();
+        }
+    }
+});
 
+close.addEventListener('click',() => {
+    hide(slider);
+    show(header);
+})
 
 toFitness.addEventListener('click', () => {
     hide(Food);
